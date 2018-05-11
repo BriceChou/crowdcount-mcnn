ef61c3250c8409618bb403e9930f08e8464021b9
Mingliang ZHOU <bricechou@gmail.com>
2018-05-11 18:12:17 +0800

    update the predict video logic

--- src/predict_video.py
+++ src/predict_video.py
@@ -2,6 +2,18 @@ from crowd_count import CrowdCounter
 import network
 import numpy as np
 import cv2
+import os
+import sys
+import time
+
+# Extend on our system's path and can load the other folder's file
+sys.path.append('..')
+import lib
+
+# Use the utf-8 coded format
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
 
 class CrowdPredictor():
     def __init__(self, model_path):
@@ -10,23 +22,6 @@ class CrowdPredictor():
         self.net.cuda()
         self.net.eval()
 
-    @staticmethod
-    def img_preprocess(img):
-        img_cp = img.copy()
-        ht = img_cp.shape[0]
-        wd = img_cp.shape[1]
-        ht_1 = (ht/16)*16
-        wd_1 = (wd/16)*16
-        if len(img.shape) == 3:
-            img_cp = cv2.cvtColor(img_cp, cv2.COLOR_BGR2GRAY)
-        
-        print(wd_1)
-        print(ht_1)
-        img_f32 = img_cp.astype(np.float32, copy=False)
-        img_f32 = cv2.resize(img_f32, (int(wd_1), int(ht_1)))
-        img_f32 = img_f32.reshape((1, 1, int(ht_1), int(wd_1)))
-        return img_f32
-
     def predict(self, img):
         img_pp = self.img_preprocess(img)
         density_map = self.net(img_pp)
@@ -34,27 +29,166 @@ class CrowdPredictor():
         count = int(np.sum(density_map))
         return count, density_map[0][0]
 
-    def predict2(self):
-        img = cv2.imread("tmp.jpg", 1)
-        return self.predict(img)
+    @staticmethod
+    def img_preprocess(img):
+        img_cp = img.copy()
+        ht = img_cp.shape[0]
+        wd = img_cp.shape[1]
+        ht_1 = (ht / 16) * 16
+        wd_1 = (wd / 16) * 16
+        if len(img.shape) == 3:
+            img_cp = cv2.cvtColor(img_cp, cv2.COLOR_BGR2GRAY)
+        img_f32 = img_cp.astype(np.float32, copy=False)
+        img_f32 = cv2.resize(img_f32, (int(wd_1), int(ht_1)))
+        img_f32 = img_f32.reshape((1, 1, int(ht_1), int(wd_1)))
+        return img_f32
 
-cp = CrowdPredictor('../final_models/mcnn_shtechB_110.h5')
-#cap = cv2.VideoCapture("../20180426.mov")
-cap = cv2.VideoCapture(-1)
-out = cv2.VideoWriter('../trainingTest.mov',cv2.VideoWriter_fourcc('M','J','P','G'), 10, (1920,1080))
-print(cap.isOpened())
-while (cap.isOpened()):
-    ret, frame = cap.read()
-    if ret == True:
+    @staticmethod
+    def line_img_area(img, start_location=(0, 0), size=(2, 2)):
+        '''
+        @parameters 
+           start_location: where start to line current image
+           size: line current image to how many part
+
+        @result
+            return : a new image matrix
+        '''
+        if size[0] < 0 or size[1] < 0:
+            print('x_size or y_size can\'t allow to set zero.')
+            return None
+
+        start_x = start_location[0]
+        start_y = start_location[1]
+        x_area_unit = int(img.shape[1] / size[0])
+        y_area_unit = int(img.shape[0] / size[1])
+
+        # Draw X axis
+        for x in range(0, size[0] - 1):
+            y_axis = start_y + y_area_unit
+            cv2.line(img, (start_location[0], y_axis),
+                     (img.shape[1], y_axis), (0, 0, 255))
+            start_y = y_axis
+
+        # Draw Y axis
+        for y in range(0, size[1] - 1):
+            x_axis = start_x + x_area_unit
+            cv2.line(img, (x_axis, start_location[1]),
+                     (x_axis, img.shape[0]), (0, 0, 255))
+            start_x = x_axis
+
+        lined_img = img.copy()
+        return lined_img
+
+    @staticmethod
+    def split_img_area(img_mat, start_location=(0, 0), size=(2, 2)):
+        '''
+        @parameters 
+           start_location: where start to split current image
+           size: split current image to how many part
+
+        @result
+            return a list for all image area
+        '''
+        if size[0] < 0 or size[1] < 0:
+            print('x_size or y_size can\'t allow to set zero.')
+            return None
+
+        start_x = start_location[0]
+        start_y = start_location[1]
+        x_area_unit = int(img_mat.shape[1] / size[0])
+        y_area_unit = int(img_mat.shape[0] / size[1])
+
+        all_area = []
+        for x in range(0, size[0]):
+            y_sum_area_unit = start_y + y_area_unit
+            for y in range(0, size[1]):
+                x_sum_area_unit = start_x + x_area_unit
+                all_area.append(img_mat[start_y:y_sum_area_unit,
+                                        start_x:x_sum_area_unit])
+                start_x = x_sum_area_unit
+
+            start_x = start_location[0]
+            start_y = y_sum_area_unit
+
+        return all_area
+
+
+# How many fps to get the iamge for original video
+fps = 30
+
+# Config the model weights path
+final_models_path = os.path.abspath('../saved_models')
+
+# Config the input image folder
+input_path = os.path.abspath('../input')
+input_fps_path = os.path.abspath('../input_original_{}'.format(fps))
+
+# Config the output image folder
+output_path = os.path.abspath('../output_original_1')
+output_fps_path = os.path.abspath('../output_original_{}'.format(fps))
+
+# Cofig the output video folder
+video_output_path = os.path.abspath('../output_video')
+video_output_fps_path = os.path.abspath('../output_video_{}'.format(fps))
+
+# Get the folder file counts
+total_num = lib.get_file_max_number(input_path)
+total_fps_num = lib.get_file_max_number(input_fps_path)
+
+# Config the output video parameter
+video_fps = 10
+video_size = (960, 540)
+fourcc = cv2.VideoWriter_fourcc('M', 'J', 'P', 'G')
+
+current_time = time.strftime("%Y%m%d%H%M%S", time.localtime())
+output_video_path = '{}/dl_crowdcount_{}.avi'.format(
+    video_output_path, current_time)
+videoWriter = cv2.VideoWriter(output_video_path, fourcc, video_fps, video_size)
+
+# initializer for current class
+cp = CrowdPredictor('{}/mcnn_20180426_994-4.h5'.format(final_models_path))
+
+# count the frame
+count_frame = 0
+cap = cv2.VideoCapture('../20180406.mov')
+rval = cap.isOpened()
+# frame_fps = 30
+while rval:
+    count_frame += 1
+    rval, frame = cap.read()
+    # if(count_frame%frame_fps == 0):
+    #     cv2.imwrite('{}/{}.jpg'.format(output_path, count_frame), frame)
+    if rval:
+        # save current frame to image
+        # img_path = '{}/{}.jpg'.format(output_path, count_frame)
+        # cv2.imwrite(img_path, frame)
+        # print('output image \033[0;32m%s\033[0m was saved. ' % (img_path))
+
         count, dens_map = cp.predict(frame)
-        print(count)
-        frame = cv2.putText(frame, "count is " + str(count), (10, 10), cv2.FONT_HERSHEY_PLAIN, 12, (0, 255, 255))
-        out.write(frame)
-        
-        if cv2.waitKey(25) & 0xFF == ord('q'):
-            break
+        new_img = cp.line_img_area(frame)
+        all_dens_map_area = cp.split_img_area(dens_map)
+        all_img_area = cp.split_img_area(new_img)
+
+        for index in range(len(all_dens_map_area)):
+            area_count = int(round(np.sum(all_dens_map_area[index])))
+            area_text = '{}'.format(area_count)
+            area_img = cv2.putText(all_img_area[index], area_text, (60, 60),
+                                   cv2.FONT_HERSHEY_DUPLEX, 2, (0, 255, 255), 2)
+
+        # To quickly finsh the task, we could not save the new image.
+        # new_img_path = '{}/{}.jpg'.format(output_path, count_frame)
+        # cv2.imwrite(new_img_path, new_img)
+        # print('New image \033[0;32m%s\033[0m was saved. And total count is %s' % (
+        #     new_img_path, count))
+
+        img2video = cv2.resize(new_img, video_size)
+        videoWriter.write(img2video)
+        print('Video Frame[\033[0;32m%s\033[0m] was saved. And total count is %s' % (
+            count_frame, count))
     else:
         break
+
 # When everything done, release the video capture object
 cap.release()
-out.release()
+videoWriter.release()
+print("All task is finished.")
